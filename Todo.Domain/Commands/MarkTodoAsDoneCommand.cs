8b2b6b9a6ae83bacf61d1738c640c32ba2a67a71
Leonardo Lima Pontes <87279897+Leonardo-Lima-Pontes@users.noreply.github.com>
2022-02-22 21:25:41 -0300

    Adicionado classe GenericCommandResult, e Command MarkTodoAsDoneCommand

---
+++ Todo.Domain/Commands/MarkTodoAsDoneCommand.cs
@@ -0,0 +1,29 @@
+using Flunt.Notifications;
+using Flunt.Validations;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class MarkTodoAsDone : Notifiable, ICommand
+    {
+        public MarkTodoAsDone() { }
+
+        public MarkTodoAsDone(Guid id, string user)
+        {
+            Id = id;
+            User = user;
+        }
+
+        public Guid? Id { get; set; }
+        public string? User { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                    .Requires()
+                    .HasMinLen(User, 6, "User", "Usuário invalido, por favor informe um usuário valido");
+            )
+        }
+    }
+}
